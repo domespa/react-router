b171ca80bd8d14de67f89370c9c3d844cf12d29e
domespa <dumiii1988@gmail.com>
2025-02-10 22:04:48 +0100

    add homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,3 +1,34 @@
 export default function Homepage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="jumbo">
+      <div className="container">
+        <div className="bgcolorwhite">
+          <h1>Benvenuto nel nostro angolo culinario!</h1>
+          <p>
+            Siamo felici di averti qui! Questo è il luogo perfetto dove
+            esplorare, scoprire e sperimentare nuove ricette. Che tu sia un
+            cuoco esperto o un principiante in cucina, il nostro obiettivo è
+            aiutarti a trovare ispirazione per ogni pasto della giornata.
+            <br />
+            Cosa troverai qui?
+            <br />- Ricette facili e veloci per ogni occasione
+            <br />- Piatti per ogni esigenza, dalle opzioni vegetariane a quelle
+            senza glutine
+            <br />- Suggerimenti e trucchi per migliorare le tue abilità
+            culinarie
+            <br />- Idee per ogni stagione, con ingredienti freschi e genuini
+            <br />
+            Inizia la tua avventura culinaria esplorando le nostre categorie di
+            ricette. Ogni piatto è pensato per essere semplice, gustoso e
+            soddisfacente. Non vediamo l'ora che tu possa provare le nostre
+            creazioni e condividerle con i tuoi cari. Siamo qui per rendere la
+            cucina un'esperienza divertente e gratificante. Prepara il grembiule
+            e lasciati ispirare dalla nostra selezione di ricette!
+            <br />
+            Buon appetito! 🍴
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
